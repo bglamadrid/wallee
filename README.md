5561631a17578945ad62884ad01ec2adacecefac
Benjamin La Madrid <bg.lamadrid@gmail.com>
2025-05-02 19:27:36 -0400

    feat: better memory algorithm
    - support bash-like parameters
      - include a help prompt
      - include verbose mode
    - use files instead of working memory
      to remember recent wallpapers
    - update readme

--- README.md
+++ README.md
@@ -5,48 +5,63 @@ Wallee was a straightforward script that automatically changed wallpaper using
 It also switched wallpapers in alphabetic order from their filenames.
 This fork is a heavy rewrite of that script.
 
-Most desktop environments do not even need a script to switch wallpapers.
-This is instead meant to be used under the i3 window manager.
+Most desktop environments (GNOME, KDE, Xfce, Cinnamon, MATE, etc) do not even
+need a script to switch wallpapers.
+Instead, this script is meant to be used under the i3 window manager.
 
-# Features
+## Features
 
 Differing from the original `wallee` script:
 
 - Pick wallpapers in a random order
-- Remember recent wallpapers to avoid repitition
-- Support content changes in the wallpapers folder
-  - No need to restart the script to add or change wallpapers
-- Command-line parameters for
-  - Wallpapers folder
-  - Time interval to switch, in seconds
-  - How many wallpapers to remember
+- Remember recent wallpapers to avoid repetition
+- Support dynamic filelist (no need to restart the script to add or change
+  wallpapers to the rotation)
+  - Three files are created in `~/.local/share/wallee/` to handle the index
+    of wallpaper files `full_index`, `available` and `recent`
+- Shell-like command-line parameters
 
-# Requirements
+## Requirements
 
-Only the Bash shell is currently supported.
-It might or might not work in other shells.
+- Only the Bash shell is currently supported.
+  It might or might not work in other shells.
+- `feh` (or an equivalent program, but then you'll have to edit the script to
+  use that)
 
-Make sure to install `feh` as well.
+## Usage
 
-# Usage
+### Syntax
 
-`wallee.sh [dir] [interval] [memory_size]`
+```bash
+wallee.sh [options]
+```
 
-- _dir_: a directory with image files in it
-    - default: `~/pictures/wallpapers`
-- _interval_: how many **seconds** before switching to another wallpaper
-    - default: 300
-- _memory_size_: how many wallpapers will be kept tabs on
-    - default: 5
+### Required options
 
-# Installation
+Option                       | Description
+-----------------------------|-------------
+`-d {}` or `--directory={}`  | The directory with image files to be used as wallpapers.
+`-n {}` or `--interval={}`   | How many seconds must pass before changing wallpapers.
+`-s {}` or `--memorysize={}` | How many recent wallpapers must be remembered. **If set to a natural number**: the script will check the entire directory on each interval and try to choose any file that is different from the latest "s" picks. **If set to -1**: the script will cycle through every single image file found in the directory.
+
+### Other options
+
+Option              | Description
+--------------------|-------------
+`-h` or `--help`    | Show this usage help in the CLI.
+`-r` or `--reset`   | Erase indexes, start memorizing wallpapers all over again.
+`-v` or `--verbose` | Explain everything that happens until an error or the `--help` option is found.
+
+## Installation
 
 Put this script somewhere in your system, and if you're using `i3`, add this
 line to your `$XDG_CONFIG_HOME/i3/config` file:
 
-`exec_always /path/to/wallee.sh [dir] [interval] [memory_size]`
+```
+exec_always /path/to/wallee.sh --directory=[dir] --interval=[interval] --memory-size=[memory_size]
+```
 
-# TODO
+## TODO
 
 Some additional features might be added in the future:
 
--- wallee.sh
+++ wallee.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-###############################################################################
-# fork of https://github.com/K-ratos/wallee
-###############################################################################
-#
-# This script will repeatedly see all image files, pick one at random, and
-# avoid repetition
-#
-# USAGE: wallee.sh [wallpaper_dir] [interval_seconds] [memory_size]
-#
-# [wallpaper_dir]: a directory with image files in it
-# [interval_seconds]: how many seconds before switching to another wallpaper
-# [memory_size]: how many wallpapers will be kept tabs on
-#
-# TODO
-# - read default configuration from within $XDG_CONFIG_HOME
-#
-###############################################################################
+print_usage_help() {
+    echo 'Fork of https://github.com/K-ratos/wallee'
+    echo 'This script will change the desktop wallpaper by randomly picking an image file'
+    echo 'from a directory. The randomness can be configured by setting a memory size.'
+    echo
+    echo 'Usage:'
+    echo 'wallee.sh [options]'
+    echo
+    echo 'Required options:'
+    echo '    -d {}, --directory={}   The directory with image files to be used as'
+    echo '                            wallpapers.'
+    echo '    -n {}, --interval={}    How many seconds must pass before changing'
+    echo '                            wallpapers.'
+    echo '    -s {}, --memorysize={}  How many recent wallpapers must be remembered.'
+    echo '                            If set to a natural number, the script will check.'
+    echo '                            the entire directory on each interval and try to'
+    echo '                            choose any file that is different from the latest'
+    echo '                            "s" picks.'
+    echo '                            If set to -1, the script will cycle through every.'
+    echo '                            single image file found in the directory.'
+    echo
+    echo 'Other options:'
+    echo '    -h, --help     Show this usage help.'
+    echo '    -r, --reset    Erase indexes, start memorizing wallpapers all over again.'
+    echo '    -v, --verbose  Explain everything that happens until an error or the --help'
+    echo '                   option is found.'
+}
+
+if [ $# -lt 1 ]; then
+    echo 'ERROR: Not enough parameters.'
+    exit 1
+fi
 
 # this outer construct explained in https://stackoverflow.com/a/169969
 (
@@ -31,26 +46,123 @@
     # and so it begins
     DEFAULT_WALLPAPERS="$HOME/pictures/wallpapers"
     DEFAULT_TIME_MINUTES=5
-    DEFAULT_TRACKED_WALLPAPERS_AMOUNT=5
+    DEFAULT_MAX_TRACKED_WALLPAPERS=-1
+    DEFAULT_STATE_DIR="$HOME/.local/share/wallee"
+    FULL_INDEX_FILE="$DEFAULT_STATE_DIR/full_index"
+    AVAILABLE_INDEX_FILE="$DEFAULT_STATE_DIR/available"
+    RECENTLY_USED_INDEX_FILE="$DEFAULT_STATE_DIR/recent"
 
     image_regex='^.*\.(jpe?g|png)$'
-    wallpapers=$DEFAULT_WALLPAPERS
+    wallpapers_directory="$DEFAULT_WALLPAPERS"
     time_interval_seconds=$((DEFAULT_TIME_MINUTES * 60))
-    total_tracked_wallpapers=$DEFAULT_TRACKED_WALLPAPERS_AMOUNT
+    max_tracked_wallpapers=$DEFAULT_MAX_TRACKED_WALLPAPERS
     how_many_recent_wallpapers=0
+    available_wallpaper_file_count=0
+    verbose=0
+    reset=0
 
-    # accept valid options, otherwise skip them
-    if [ -n "$1" ]
+    # parse program options
+    arg_i=0
+    shortarg=
+    for arg in "$@"; do
+        (( arg_i++ ));
+        if [ -z "$arg" ]; then continue; fi
+        if [ $verbose -eq 1 ]; then echo "[wallee.sh] arg=$arg"; fi
+        if [ "$arg" = '-r' ] || [ "$arg" = '--reset' ];
+        then
+            reset=1
+            continue
+        fi
+        if [ "$arg" = '-v' ] || [ "$arg" = '--verbose' ]
+        then
+            verbose=1
+            echo "[wallee.sh] == VERBOSE MODE =="
+            continue
+        fi
+        if [ "$arg" = '-h' ] || [ "$arg" = '--help' ]; then print_usage_help; exit; fi
+        longarg=$(echo "$arg" | sed -E 's/^--([a-z]{4,})=.+$/\1/; t; Q; p')
+        if [ -n "$longarg" ]
+        then
+            longval=$(echo "$arg" | sed -E 's/^--'"$longarg"'=(.+)$/\1/; t; Q; p')
+            if [ $verbose -eq 1 ]
+            then
+                echo "[wallee.sh] longarg=$longarg"
+                echo "[wallee.sh] longval=$longval"
+            fi
+            case "$longarg" in
+            directory)
+                wallpapers_directory="$longval"
+                ;;
+            interval)
+                time_interval_seconds=$longval
+                ;;
+            memorysize)
+                max_tracked_wallpapers=$longval
+                ;;
+            esac
+            longarg=
+            continue
+        elif [ -z "$shortarg" ]
+        then
+            shortarg="$(echo "$arg" | sed -E 's/^-(.)$/\1/; t; Q; p')"
+            if [ -z "$shortarg" ]
+            then
+                echo '[wallee.sh] ERROR: Parameter order is wrong.'
+                exit 1
+            fi
+            continue
+        else
+            case "$shortarg" in
+            d)
+                wallpapers_directory="$arg"
+                ;;
+            n)
+                time_interval_seconds=$arg
+                ;;
+            s)
+                max_tracked_wallpapers=$arg
+                ;;
+            esac
+            if [ $verbose -eq 1 ]
+            then
+                echo "[wallee.sh] shortarg=$shortarg"
+                echo "[wallee.sh] value=$arg"
+            fi
+            shortarg=
+        fi
+    done
+
+    # debug options
+    if [ $verbose -eq 1 ]
     then
-        wallpapers="$1"
+        echo "[wallee.sh] wallpapers_directory=$wallpapers_directory"
+        echo "[wallee.sh] time_interval_seconds=$time_interval_seconds"
+        echo "[wallee.sh] max_tracked_wallpapers=$max_tracked_wallpapers"
     fi
-    if [ -n "$2" ] && [[ "$2" =~ ^[0-9]{1,2}$ ]] && [ $2 -gt 0 ]
+
+    if [ ! -d "$DEFAULT_STATE_DIR" ]
     then
-        time_interval_seconds=$2
+        mkdir -p "$DEFAULT_STATE_DIR"
     fi
-    if [ -n "$3" ] && [[ "$3" =~ ^[0-9]{1,2}$ ]] && [ $3 -ge 0 ]
+
+    if [ ! -f "$FULL_INDEX_FILE" ] || [ ! -s "$FULL_INDEX_FILE" ] || \
+       [ ! -f "$AVAILABLE_INDEX_FILE" ] || \
+       [ ! -f "$RECENTLY_USED_INDEX_FILE" ] || \
+       [ $reset -eq 1 ]
     then
-        total_tracked_wallpapers=$3
+        if [ $verbose -eq 1 ]; then echo "[wallee.sh] Creating new cache files"; fi
+        touch "$FULL_INDEX_FILE" \
+              "$AVAILABLE_INDEX_FILE" \
+              "$RECENTLY_USED_INDEX_FILE"
+        # cache all image files in wallpapers directory
+        find "$wallpapers_directory" -maxdepth 1 \
+                           -type f,l \
+                           -regextype egrep \
+                           -regex $image_regex \
+                           -printf '%f\n' > "$FULL_INDEX_FILE"
+        if [ $verbose -eq 1 ]; then echo "[wallee.sh] Found $(cat "$FULL_INDEX_FILE" | wc -l) wallpaper files"; fi
+        cp "$FULL_INDEX_FILE" "$AVAILABLE_INDEX_FILE"
+        cp /dev/null "$RECENTLY_USED_INDEX_FILE"
     fi
 
     # this array will keep tabs on recent wallpapers
@@ -59,58 +171,57 @@
     # main loop
     while true
     do
-        # cache all image files in directory
-        # this might be expensive but it works pretty good for this purpose
-        files=$(find "$wallpapers" -maxdepth 1 -type f -regextype egrep -regex $image_regex -printf '%f\n')
-
-        # are there actually any image files in the directory?
-        file_count=$(echo "$files" | wc -l)
-        if [ $file_count = 0 ]
+        # are there actually any image files to use?
+        available_wallpaper_file_count=$(cat "$AVAILABLE_INDEX_FILE" | wc -l)
+        if [ $verbose -eq 1 ]; then echo "[wallee.sh] There are $available_wallpaper_file_count wallpapers available to select"; fi
+        if [ $available_wallpaper_file_count -eq 0 ]
         then
-            echo "No image files found in $wallpapers"
-            sleep $time_interval_seconds
-            continue # don't hurry to kill the script if not
+            cp "$FULL_INDEX_FILE" "$AVAILABLE_INDEX_FILE"
+            cp /dev/null "$RECENTLY_USED_INDEX_FILE"
+            continue
         fi
+
+        mapfile -t $recent_wallpapers < "$RECENTLY_USED_INDEX_FILE"
+        how_many_recent_wallpapers=${#recent_wallpapers[@]}
+        if [ $verbose -eq 1 ]; then echo "[wallee.sh] In recent memory, $how_many_recent_wallpapers wallpapers have been used"; fi
 
         # pick a random wallpaper
-        random_index=$((1 + ($RANDOM % $file_count)))
-        random_file=$(echo "$files" | head -"$random_index" | tail -1)
+        random_index=$((1 + ($RANDOM % $available_wallpaper_file_count)))
+        random_file="$(cat "$AVAILABLE_INDEX_FILE" | head -"$random_index" | tail -1)"
+        if [ $verbose -eq 1 ]; then echo "[wallee.sh] Picked file no. $random_index from available list: $random_file"; fi
 
-        # was this wallpaper picked too recently? if so, try again
-        if [ $how_many_recent_wallpapers -gt 0 ]
+        if [ $max_tracked_wallpapers -le -1 ]
+        # remember as many wallpapers as there are
         then
-            compare_i=1
-            while [ $compare_i -le $how_many_recent_wallpapers ]
-            do
-                this_wallpaper=${recent_wallpapers[$compare_i]}
-                (( compare_i++ ))
-                if [ "$random_file" = "$this_wallpaper" ]
-                then
-                    continue 2
-                fi
-            done
-        fi
-
-        # remember the most recent wallpapers
-        if [ $how_many_recent_wallpapers -lt $total_tracked_wallpapers ]
-        then
-            # push all until the array is filled
-            (( how_many_recent_wallpapers++ ))
-            recent_wallpapers[$how_many_recent_wallpapers]="$random_file"
+            if [ $how_many_recent_wallpapers -ge $available_wallpaper_file_count ]
+            # when there be more wallpapers in memory than in the directory
+            then
+                cp "$FULL_INDEX_FILE" "$AVAILABLE_INDEX_FILE"
+                cp /dev/null "$RECENTLY_USED_INDEX_FILE"
+            fi
+            sed -i $random_index'd' "$AVAILABLE_INDEX_FILE"
+            printf "$random_file\n" >> "$RECENTLY_USED_INDEX_FILE"
         else
-            # then keep track "first-in, last-out"-style
-            remember_i=$total_tracked_wallpapers
-            while [ $remember_i -gt 0 ]
-            do
-                remember_next=$(( remember_i - 1 ))
-                recent_wallpapers[$remember_i]=${recent_wallpapers[$remember_next]}
-                (( remember_i-- ))
-            done
-            recent_wallpapers[1]="$random_file"
+            if [ $how_many_recent_wallpapers -lt $max_tracked_wallpapers ]
+            then
+                printf "$random_file\n" >> "$RECENTLY_USED_INDEX_FILE"
+            else
+                # when max amount of tracked wallpapers has been reached,
+                # keep track FILO-style
+                remember_i=$max_tracked_wallpapers
+                while [ $remember_i -gt 0 ]
+                do
+                    remember_next=$(( remember_i - 1 ))
+                    recent_wallpapers[$remember_i]=${recent_wallpapers[$remember_next]}
+                    (( remember_i-- ))
+                done
+                recent_wallpapers[1]="$random_file"
+                printf "%s\n" "${recent_wallpapers[@]}" > "$RECENTLY_USED_INDEX_FILE"
+            fi
         fi
 
         # actually set the wallpaper and wait some time before going at it again
-        feh --no-fehbg --bg-fill "$wallpapers/$random_file"
+        feh --no-fehbg --bg-fill "$wallpapers_directory/$random_file"
         sleep $time_interval_seconds
     done
 
